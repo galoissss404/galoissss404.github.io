976e424d7759423e260f97418731724eb262381d
galoissss404 <galoissss404@users.noreply.github.com>
2022-06-21 12:43:23 +0000

    jekyll build from Action 4d00094e2267f4109e58a20dfcee9f3743bf9bd1

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome to $$E=mc^2$$!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. \\(E=mc^2\\) You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site...","categories": ["jekyll","update"],
+        "tags": ["start"],
+        "url": "/jekyll/update/2022/06/21/welcome-to-jekyll.html",
+        "teaser": null
+      }]
